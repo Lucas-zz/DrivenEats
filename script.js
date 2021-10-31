b8d9b0789af034a2caa0ee9793bd7ca284152614
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2021-10-31 14:16:45 -0300

    little changes on identation and z-index in header and footer

--- script.js
+++ script.js
@@ -16,104 +16,114 @@ let valorSobremesa = 0;
 let valorTotal = 0;
 
 function toggle1(comida) {
-    const selecionado = document.querySelector(".borda1");
-    const check = comida.querySelector(".checkmark");
+  const selecionado = document.querySelector(".borda1");
+  const check = comida.querySelector(".checkmark");
 
-    if (selecionado !== null) {
-        const mark = selecionado.querySelector(".checkmark");
-        selecionado.classList.remove("borda1");
-        mark.classList.add("noDisplay");
+  if (selecionado !== null) {
+    const mark = selecionado.querySelector(".checkmark");
+    selecionado.classList.remove("borda1");
+    mark.classList.add("noDisplay");
 
-        valorComida = 0;    //reseta o valor com a troca de itens;
-    }
-    comida.classList.toggle("borda1");
-    check.classList.toggle("noDisplay");
+    valorComida = 0; //reseta o valor com a troca de itens;
+  }
+  comida.classList.toggle("borda1");
+  check.classList.toggle("noDisplay");
 
-    let valor = parseFloat(comida.querySelector(".valor").innerHTML.replace(",", "."));
-    valorComida = valor;    //atribui o valor do item a uma varíavel para futuro cáculo de valores;
+  let valor = parseFloat(
+    comida.querySelector(".valor").innerHTML.replace(",", ".")
+  );
+  valorComida = valor; //atribui o valor do item a uma varíavel para futuro cáculo de valores;
 
-    tipoComida = comida.querySelector(".nome").innerHTML; // atribui o nome do item selecionado a uma variável;
-    comidaSelecionada = true;   // confirmação da seleção de um item;
+  tipoComida = comida.querySelector(".nome").innerHTML; // atribui o nome do item selecionado a uma variável;
+  comidaSelecionada = true; // confirmação da seleção de um item;
 
-    finalizarPedido();
+  finalizarPedido();
 }
-
 
 function toggle2(bebida) {
-    const selecionado = document.querySelector(".borda2");
-    const check = bebida.querySelector(".checkmark");
+  const selecionado = document.querySelector(".borda2");
+  const check = bebida.querySelector(".checkmark");
 
-    if (selecionado !== null) {
-        const mark = selecionado.querySelector(".checkmark");
-        selecionado.classList.remove("borda2");
-        mark.classList.add("noDisplay");
+  if (selecionado !== null) {
+    const mark = selecionado.querySelector(".checkmark");
+    selecionado.classList.remove("borda2");
+    mark.classList.add("noDisplay");
 
-        valorBebida = 0;
-    }
-    bebida.classList.toggle("borda2");
-    check.classList.toggle("noDisplay");
+    valorBebida = 0;
+  }
+  bebida.classList.toggle("borda2");
+  check.classList.toggle("noDisplay");
 
-    let valor = parseFloat(bebida.querySelector(".valor").innerHTML.replace(",", "."));
-    valorBebida = valor;
+  let valor = parseFloat(
+    bebida.querySelector(".valor").innerHTML.replace(",", ".")
+  );
+  valorBebida = valor;
 
-    tipoBebida = bebida.querySelector(".nome").innerHTML;
-    bebidaSelecionada = true;
+  tipoBebida = bebida.querySelector(".nome").innerHTML;
+  bebidaSelecionada = true;
 
-    finalizarPedido();
+  finalizarPedido();
 }
-
 
 function toggle3(sobremesa) {
-    const selecionado = document.querySelector(".borda3");
-    const check = sobremesa.querySelector(".checkmark");
+  const selecionado = document.querySelector(".borda3");
+  const check = sobremesa.querySelector(".checkmark");
 
-    if (selecionado !== null) {
-        const mark = selecionado.querySelector(".checkmark");
-        selecionado.classList.remove("borda3");
-        mark.classList.add("noDisplay");
+  if (selecionado !== null) {
+    const mark = selecionado.querySelector(".checkmark");
+    selecionado.classList.remove("borda3");
+    mark.classList.add("noDisplay");
 
-        valorSobremesa = 0;
-    }
-    sobremesa.classList.toggle("borda3");
-    check.classList.toggle("noDisplay");
+    valorSobremesa = 0;
+  }
+  sobremesa.classList.toggle("borda3");
+  check.classList.toggle("noDisplay");
 
-    let valor = parseFloat(sobremesa.querySelector(".valor").innerHTML.replace(",", "."));
-    valorSobremesa = valor;
+  let valor = parseFloat(
+    sobremesa.querySelector(".valor").innerHTML.replace(",", ".")
+  );
+  valorSobremesa = valor;
 
-    tipoSobremesa = sobremesa.querySelector(".nome").innerHTML;
-    sobremesaSelecionada = true;
+  tipoSobremesa = sobremesa.querySelector(".nome").innerHTML;
+  sobremesaSelecionada = true;
 
-    finalizarPedido();
+  finalizarPedido();
 }
-
 
 function finalizarPedido() {
-    const button = document.querySelector(".botao button");
-    if (comidaSelecionada === true && bebidaSelecionada === true && sobremesaSelecionada === true) {
-        button.classList.remove("disabled");
-        button.classList.add("enabled");
-        button.innerHTML = "Finalizar pedido";
-    } else {
-        button.classList.remove("enabled");
-        button.classList.add("disabled");
-    }
-    valorTotal = valorComida + valorBebida + valorSobremesa;
+  const button = document.querySelector(".botao button");
+  if (
+    comidaSelecionada === true &&
+    bebidaSelecionada === true &&
+    sobremesaSelecionada === true
+  ) {
+    button.classList.remove("disabled");
+    button.classList.add("enabled");
+    button.innerHTML = "Finalizar pedido";
+  } else {
+    button.classList.remove("enabled");
+    button.classList.add("disabled");
+  }
+  valorTotal = valorComida + valorBebida + valorSobremesa;
 }
 
-
 function fechamentoWP() {
-    let msg = String;
+  let msg = String;
 
-    let total = valorTotal.toFixed(2);
+  let total = valorTotal.toFixed(2);
 
-    if (comidaSelecionada === true && bebidaSelecionada === true && sobremesaSelecionada === true) {
-        let name = prompt("Qual é o seu nome?");
-        let address = prompt("Qual é o seu endereço?");
+  if (
+    comidaSelecionada === true &&
+    bebidaSelecionada === true &&
+    sobremesaSelecionada === true
+  ) {
+    let name = prompt("Qual é o seu nome?");
+    let address = prompt("Qual é o seu endereço?");
 
-        msg = encodeURIComponent(
-            `Olá, gostaria de fazer o pedido:\n- Prato: ${tipoComida}\n- Bebida: ${tipoBebida}\n- Sobremesa: ${tipoSobremesa}\nTotal: R$ ${total}
+    msg = encodeURIComponent(
+      `Olá, gostaria de fazer o pedido:\n- Prato: ${tipoComida}\n- Bebida: ${tipoBebida}\n- Sobremesa: ${tipoSobremesa}\nTotal: R$ ${total}
             \nNome: ${name}\nEndereço: ${address}`
-        );
-        window.open(` https://wa.me/+5547996988923?text=${msg}`);
-    }
+    );
+    window.open(` https://wa.me/+5547996988923?text=${msg}`);
+  }
 }
